ffa932c0a96e03280309347410f7b3ba94525642
A1thernex <135517963+A1thernex@users.noreply.github.com>
2025-01-21 22:23:22 +0400

    Update loader.lua

--- bloxsense/loader.lua
+++ bloxsense/loader.lua
@@ -1,14 +1,15 @@
-repeat task.wait() until game:IsLoaded()
-
 local githublink = "https://raw.githubusercontent.com/A1thernex/random/refs/heads/main/bloxsense/"
 
 local games = {
-    ["18976527873"] = githublink .. "cbe.lua",
-    ["17586052747"] = githublink .. "cbrv2.lua"
+    ["18976527873"] = "cbe.lua",
+    ["17586052747"] = "cbrv2.lua",
+    ["12501200809"] = "csgo%20modded.lua" 
 }
 
 local link = games[tostring(game.PlaceId)]
 
 if link then
-    loadstring(game:HttpGet(link))()
+    loadstring(game:HttpGet(githublink .. link))()
+else
+    warn("[bloxsense] game not supported")
 end
