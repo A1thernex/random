af9d97e83bd7ab218e778c9aa3734ec85b6feefa
A1thernex <135517963+A1thernex@users.noreply.github.com>
2025-02-07 22:22:41 +0400

    Update luautools.lua

--- luautools.lua
+++ luautools.lua
@@ -10,7 +10,9 @@
 env = getgenv and getgenv() or getfenv(0)
 cloneref = cloneref or function(instance) return instance end
 
-local getServ = game.FindService
+local game = game
+
+local getServ = game.GetService
 
 local function makeFunction(aliases: table, callback)
 	for _, alias in pairs(aliases) do
@@ -18,22 +20,20 @@ local function makeFunction(aliases: table, callback)
 	end
 end
 
-local function cframeToVec3(position: CFrame)
-    return Vector3.new(position.Position.X, position.Position.Y, position.Position.Z)
-end
-
 makeFunction({"gs", "getService", "GetService", "getservice"}, function(service: Instance)
 	return cloneref(getServ(game, service))
 end)
 
-local players, stats, tpService, uis, httpserv, tween, rs = gs("Players"), gs("Stats"), gs("TeleportService"), gs("UserInputService"), gs("HttpService"), gs("TweenService"), gs("RunService")
+local players, stats, tpService, uis, httpServ, tween, rs = gs("Players"), gs("Stats"), gs("TeleportService"), gs("UserInputService"), gs("HttpService"), gs("TweenService"), gs("RunService")
 local camera = workspace.CurrentCamera
-local instNew, drawNew, color3Hsv, vec2new, rand, huge, floor, clock, date, tweenInfo, strFind, tblInsert, cframeNew, ceil = Instance.new, Drawing and Drawing.new or nil, Color3.fromHSV, Vector2.new, math.random, math.huge, math.floor, os.clock, os.date, TweenInfo.new, string.find, table.insert, math.ceil
+local instNew, drawNew, color3Hsv, vec2New, vec3New, rand, huge, floor, clock, date, tweenInfo, strFind, strSub, tblInsert, cframeNew, ceil, tick = Instance.new, Drawing and Drawing.new or nil, Color3.fromHSV, Vector2.new, Vector3.new, math.random, math.huge, math.floor, os.clock, os.date, TweenInfo.new, string.find, string.sub, table.insert, math.ceil, tick
 local lplr = players.LocalPlayer
 local worldToViewport, getParts = camera.WorldToViewportPoint, camera.GetPartsObscuringTarget
-local plrTbl, plrStr = {}, ""
-local charSet, ranstr = "", ""
-local lastTime, start, frameTbl = nil, nil, {}
+local getMouseLocation = uis.GetMouseLocation
+local getChildren, getDescendants = game.GetChildren, game.GetDescendants
+local findFirstChild, findFirstChildOfClass = game.FindFirstChild, game.FindFirstChildOfClass
+local getFullName = game.GetFullName
+local isA = game.IsA
 local loops = {}
 
 local scripts = {
@@ -46,13 +46,17 @@ local scripts = {
 	["ss"] = "https://raw.githubusercontent.com/78n/SimpleSpy/refs/heads/main/SimpleSpySource.lua"
 }
 
-makeFunction({"isalive", "alive", "isAlive", "IsAlive", "Alive"}, function(plr: Player)
-	plr = plr or lplr
-	plrchar = plr:IsA("Model") and plr or plr.Character
+local function cframeToVec3(position: CFrame)
+    return vec3New(position.Position.X, position.Position.Y, position.Position.Z)
+end
 
-	if plrchar then
-		if plrchar:FindFirstChild("Humanoid") and plrchar:FindFirstChild("HumanoidRootPart") then
-			return true, plrchar
+makeFunction({"isalive", "alive", "isAlive", "IsAlive", "Alive"}, function(plr: Player)
+	local plr = plr or lplr
+	local plrChar = isA(plr, "Model") and plr or plr.Character
+
+	if plrChar then
+		if findFirstChild(plrChar, "Humanoid") and findFirstChild(plrChar, "HumanoidRootPart") then
+			return true, plrChar
 		end
 	end
 	
@@ -60,39 +64,38 @@ makeFunction({"isalive", "alive", "isAlive", "IsAlive", "Alive"}, function(plr: 
 end)
 
 makeFunction({"gethrp", "getHrp", "GetHrp", "GetHRP"}, function(plr: Player)
-	plr = plr or lplr
-	
-	plrAlive, char = alive(plr)
+	local plr = plr or lplr
+	local plrAlive, char = alive(plr)
 
 	if plrAlive then
-		return char:FindFirstChild("HumanoidRootPart") and char.HumanoidRootPart
+		return findFirstChild(char, "HumanoidRootPart") and char.HumanoidRootPart
 	end
 end)
 
 makeFunction({"gethum", "gethumanoid", "getHum", "getHumanoid", "GetHum", "GetHumanoid"}, function(plr: Player)
-	plr = plr or lplr
-
-	plrAlive, char = alive(plr)
+	local plr = plr or lplr
+	local plrAlive, char = alive(plr)
 
 	if plrAlive then
-		return char:FindFirstChild("Humanoid") and char.Humanoid
+		return findFirstChild(char, "Humanoid") and char.Humanoid
 	end
 end)
 
 makeFunction({"gettool", "getTool", "GetTool"}, function(plr: Player)
-	plr = plr or lplr
-	
-	plrAlive, char = alive(plr)
-	tool = nil
+	local plr = plr or lplr
+	local plrAlive, char = alive(plr)
+	local tool = nil
 	
 	if plrAlive then
-		tool = char:FindFirstChildOfClass("Tool") or nil
+		tool = findFirstChildOfClass(char, "Tool") or nil
 	end
 	
 	return tool, tool.Name
 end)
 
 makeFunction({"getplrs", "getPlrs", "GetPlrs", "getplayers", "getPlayers", "GetPlayers"}, function()
+	local plrTbl, plrStr = {}, ""
+		
 	for _, plr in pairs(players:GetPlayers()) do
 		if plr == lplr then
 			plrTbl["LocalPlayer"] = plr
@@ -115,6 +118,7 @@ end)
 
 makeFunction({"newinstance", "newinst", "newInstance", "newInst", "makeinst", "makeinstance", "makeInst", "makeInstance", "NewInst", "NewInstance", "MakeInst", "MakeInstance"}, function(instanceType: string, properties: table)
 	local instance = instNew(instanceType)
+	
 	for property, value in pairs(properties) do
 		instance[property] = value
 	end
@@ -123,20 +127,21 @@ makeFunction({"newinstance", "newinst", "newInstance", "newInst", "makeinst", "m
 end)
 
 makeFunction({"draw", "newdrawing", "makedrawing", "Draw", "newDrawing", "makeDrawing", "NewDrawing", "MakeDrawing"}, function(drawingType: string, properties: table)
-	assert(drawnew ~= nil, "Failed to create a new Drawing object! Reason: Drawing.new is not supported by your exploit.")
+	assert(drawNew ~= nil, "Failed to create a new Drawing object! Reason: Drawing.new is not supported by your exploit.")
 	
-	local newdraw = drawNew(drawingType)
+	local newDraw = drawNew(drawingType)
+	
 	for property, value in pairs(properties) do
-		newdraw[property] = value
+		newDraw[property] = value
 	end
 	
-	return newdraw
+	return newDraw
 end)
 
 makeFunction({"randstr", "randomstr", "randomstring", "randStr", "randomStr", "randomString", "RandStr", "RandomStr", "RandomString"}, function(length: number, settings: table)
-	settings = settings or {}
-	letters = settings.letters == nil and true or settings.letters
-	ranstr = ""
+	local settings = settings or {}
+	local charSet, ranStr, index, letter = "", "", 0, ""
+	local letters = settings.letters == nil and true or settings.letters
 
 	if letters then
 		charSet = charSet .. "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -149,16 +154,16 @@ makeFunction({"randstr", "randomstr", "randomstring", "randStr", "randomStr", "r
 	end
 	
 	for i = 0, length do
-		local index = rand(#charSet)
-		
-		local letter = string.sub(charSet, index, index)
-		ranstr = ranstr .. letter
+		index = rand(#charSet)
+		letter = strSub(charSet, index, index)
+			
+		ranStr = ranStr .. letter
 	end
 	
-	return ranstr
+	return ranStr
 end)
 
-makeFunction({"getrnbw", "getRnbw", "GetRnbw", "getrainbow", "getRainbow", "GetRainbow", "getrainbowcolor", "getRainbowColor", "GetRainbowColor", ""}, function()
+makeFunction({"getrnbw", "getRnbw", "GetRnbw", "getrainbow", "getRainbow", "GetRainbow", "getrainbowcolor", "getRainbowColor", "GetRainbowColor"}, function()
 	return color3Hsv(tick() % 1 / 1, 1, 1)
 end)
 
@@ -186,7 +191,7 @@ makeFunction({"closest", "Closest", "getclosest", "getClosest", "GetClosest", "g
 				end
 			end
 		else
-			for _, child in pairs(instance:GetChildren()) do
+			for _, child in pairs(getChildren(instance)) do
 				if lplrAlive and child.HumanoidRootPart then
 					local magnitude = (child.HumanoidRootPart.Position - lplrChar.HumanoidRootPart.Position).magnitude
 					
@@ -204,7 +209,7 @@ makeFunction({"closest", "Closest", "getclosest", "getClosest", "GetClosest", "g
 				if plrAlive and lplrAlive then
 					-- taken from exunys
 					local onScreen, pos = visibletocam(plrChar)
-					local distance = (vec2new(uis:GetMouseLocation().X, uis:GetMouseLocation().Y) - vec2new(pos.X, pos.Y)).magnitude
+					local distance = (vec2New(getMouseLocation(uis).X, getMouseLocation(uis).Y) - vec2New(pos.X, pos.Y)).magnitude
 
 					if distance < mouseDistance and onScreen then
 						closest = plr 
@@ -212,10 +217,10 @@ makeFunction({"closest", "Closest", "getclosest", "getClosest", "GetClosest", "g
 				end
 			end
 		else
-			for _, child in pairs(instance:GetChildren()) do
+			for _, child in pairs(getChildren(instance)) do
 				if lplrAlive then
 					local onScreen, pos = visibletocam(child)
-					local distance = (vec2new(uis:GetMouseLocation().X, uis:GetMouseLocation().Y) - vec2new(pos.X, pos.Y)).magnitude
+					local distance = (vec2New(uis:GetMouseLocation().X, uis:GetMouseLocation().Y) - vec2New(pos.X, pos.Y)).magnitude
 					if distance < mouseDistance and onScreen then
 						closest = child
 					end
@@ -258,6 +263,7 @@ end)
 
 makeFunction({"isoncam", "isOnCam", "IsOnCam", "visibletocam", "visibleToCam", "VisibleToCam", "visibletocamera", "visibleToCamera", "VisibleToCamera", "visibleoncamera", "visibleOnCamera", "VisibleOnCamera", "isoncamera", "isOnCamera", "IsOnCamera"}, function(plr: Player)
 	if plr == lplr then return end
+		
 	local plrAlive, plrChar = alive(plr)
 
 	if plrAlive then
@@ -316,17 +322,17 @@ makeFunction({"getcfg", "getCfg", "GetCfg", "getconfig", "getConfig", "GetConfig
     local configinst = {}
     
     function configinst:saveData(data: table) 
-        data = httpserv:JSONEncode(data)
+        data = httpServ:JSONEncode(data)
         writefile(path, data)
     end
     
     function configinst:readData()
-        local parsed = httpserv:JSONDecode(readfile(path))
+        local parsed = httpServ:JSONDecode(readfile(path))
         return parsed
     end
     
     function configinst:getValue(value: string)
-        local parsed = httpserv:JSONDecode(readfile(path))
+        local parsed = httpServ:JSONDecode(readfile(path))
         
         if parsed[value] then
             return parsed[value]
@@ -334,13 +340,13 @@ makeFunction({"getcfg", "getCfg", "GetCfg", "getconfig", "getConfig", "GetConfig
     end
     
     function configinst:editValue(value: string, newValue: any)
-        local parsed = httpserv:JSONDecode(readfile(path))
+        local parsed = httpServ:JSONDecode(readfile(path))
         
         if parsed[value] then
             parsed[value] = newValue
         end
         
-        parsed = httpserv:JSONEncode(parsed)
+        parsed = httpServ:JSONEncode(parsed)
         writefile(path, parsed)
     end
     
@@ -362,7 +368,7 @@ makeFunction({"tptoposition", "tptoPosition", "tpToPosition", "TpToPosition", "t
     local plrAlive, char = alive()
     
     if plrAlive then
-        local hrp = gethrp()
+        local hrp = getHrp()
         
         if tpType == "Normal" then
             if typeof(position) == "Vector3" then
@@ -399,7 +405,7 @@ makeFunction({"tptoplayer", "tptoPlayer", "tpToPlayer", "TpToPlayer", "teleportt
         if method == "Normal" then
             getHrp().CFrame = getHrp(player).CFrame
         elseif method == "Tween" then
-            local time = math.ceil((plrChar.Head.Position - cframeToVec3(position)).Magnitude / 3 + 0.5) / 50
+            local time = ceil((plrChar.Head.Position - cframeToVec3(position)).Magnitude / 3 + 0.5) / 50
             local info = tweenInfo(time, Enum.EasingStyle.Linear)
             
             tween:Create(getHrp(), info, {CFrame = getHrp(player).CFrame})
@@ -421,7 +427,7 @@ makeFunction({"tptopart", "tptoPart", "tpToPart", "TpToPart", "teleporttopart", 
         if method == "Normal" then
             getHrp().CFrame = part.CFrame
         elseif method == "Tween" then
-            local time = math.ceil((plrChar.Head.Position - cframeToVec3(part.CFrame)).Magnitude / 3 + 0.5) / 25
+            local time = ceil((plrChar.Head.Position - cframeToVec3(part.CFrame)).Magnitude / 3 + 0.5) / 25
             local info = tweenInfo(time, Enum.EasingStyle.Linear)
             
             tween:Create(getHrp(), info, {CFrame = part.CFrame}):Play()
@@ -445,27 +451,26 @@ makeFunction({"checkprop", "checkProp", "CheckProp", "checkproperty", "checkProp
 end)
 
 makeFunction({"searchobj", "searchObj", "SearchObj", "searchobject", "searchObject", "SearchObject", "findobj", "findObj", "FindObj", "findobject", "findObject", "FindObject"}, function(args: table)
-    local data = {
+    	local data = {
 		origin = args.origin or args.Origin or nil, -- where to search from
 		searchMode = args.searchmode or args.searchMode or args.SearchMode or "Children", -- basically :GetChildren() or :GetDescendants()
 		object = args.object or args.Object, -- the object to search for
-		-- apparently can crash lol
+		-- extrafilters can apparently crash lol
 		extraFilters = args.extrafilters or args.extraFilters or args.ExtraFilters, -- find by property values, e.g. filterValues = {["Name"] = "ObjectName", ["Color3"] = Color3.new()}
 		output = args.output or args.Output or false -- whether to output the objects found or not
 	}
-
 	local objTbl = {}
 	local index = 1
 
 	if not data.origin then
-		for _, obj in game:GetDescendants() do
+		for _, obj in getDescendants(game) do
 			if data.extraFilters or #data.extraFilters > 0 then
 				for prop, val in data.extraFilters do
 					if checkProperty(obj, prop) and obj[prop] == val then
 						tblInsert(objTbl, obj)
 
 						if data.output then
-							print("[" .. index .. "] Found matching object: " .. obj.Name .. ", Path: " .. obj:GetFullName())
+							print("[" .. index .. "] Found matching object: " .. obj.Name .. ", Path: " .. getFullName(obj))
 							index += 1
 						end
 					end
@@ -485,25 +490,25 @@ makeFunction({"searchobj", "searchObj", "SearchObj", "searchobject", "searchObje
 			return objTbl
 		end
 
-		for _, obj in game:GetDescendants() do
-			if obj.Name == data.object or obj:IsA(data.object) or strFind(obj.Name, data.object) then
+		for _, obj in getDescendants(game) do
+			if obj.Name == data.object or isA(obj, data.object) or strFind(obj.Name, data.object) then
 				tblInsert(objTbl, obj)
 
 				if data.output then
-					print("[" .. index .. "] Found matching object: " .. obj.Name .. ", Path: " .. obj:GetFullName())
+					print("[" .. index .. "] Found matching object: " .. obj.Name .. ", Path: " .. getFullName(obj))
 					index += 1
 				end
 			end
 		end
 	else
-		for _, obj in (data.searchMode == "Descendants" and data.origin:GetDescendants() or data.origin:GetChildren()) do
+		for _, obj in (data.searchMode == "Descendants" and getDescendants(data.origin) or getChildren(data.origin)) do
 			if data.extraFilters or #data.extraFilters > 0 then
 				for prop, val in data.extraFilters do
 					if checkProperty(obj, prop) and obj[prop] == val then
 						tblInsert(objTbl, obj)
 
 						if data.output then
-							print("[" .. index .. "] Found matching object: " .. obj.Name .. ", Path: " .. obj:GetFullName())
+							print("[" .. index .. "] Found matching object: " .. obj.Name .. ", Path: " .. getFullName(obj))
 							index += 1
 						end
 					end
@@ -523,12 +528,12 @@ makeFunction({"searchobj", "searchObj", "SearchObj", "searchobject", "searchObje
 			return objTbl
 		end
 
-		for _, obj in (data.searchMode == "Descendants" and data.origin:GetDescendants() or data.origin:GetChildren()) do
-			if obj.Name == data.object or obj:IsA(data.object) or strFind(obj.Name, data.object) then
+		for _, obj in (data.searchMode == "Descendants" and getDescendants(data.origin) or getChildren(data.origin)) do
+			if obj.Name == data.object or isA(obj, data.object) or strFind(obj.Name, data.object) then
 				tblInsert(objTbl, obj)
 
 				if data.output then
-					print("[" .. index .. "] Found matching object: " .. obj.Name .. ", Path: " .. obj:GetFullName())
+					print("[" .. index .. "] Found matching object: " .. obj.Name .. ", Path: " .. getFullName(obj))
 					index += 1
 				end
 			end
@@ -569,7 +574,7 @@ makeFunction({"disconnectrsloop, disconnectrsLoop", "disconnectRsLoop", "Disconn
     end
 end)
 
-makeFunction({"listrsloops", "listrsLoops", "listRsLoops", "ListRsLoops", "listrunserviceloops", "listrunserviceLoops", "listrunServiceLoops", "listRunServiceLoops", "ListRunServiceLoops"}, function()
+makeFunction({"listrsloops", "listrsLoops", "listRsLoops", "ListRsLoops"}, function()
     local string = ""
     
     for id, _ in loops do
